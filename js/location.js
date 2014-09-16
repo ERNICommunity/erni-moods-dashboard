68ff3a3776108cdf4233c76e83172e05875b494c
Ján Neščivera <jan.nescivera@gmail.com>
2014-09-16 11:28:05 +0200

    improved marker presentation

--- js/location.js
+++ js/location.js
@@ -5,29 +5,34 @@
     
     app.controller('LocationController', ['$scope', '$http', function LoginCtrl($scope, $http) {
 
-        $scope.map = {center: {latitude: 46.8944, longitude: 8.1723 }, zoom: 3, bounds: {}};
+        $scope.map = {center: {latitude: 46.8944, longitude: 8.1723 }, zoom: 5, bounds: {}};
         $scope.options = {scrollwheel: true};
         $scope.moodMarkers = [];
-        
-//        $scope.markersEvents = {
-//          click: function (gMarker, eventName, model) {
-//            if(model.$id){
-//              model = model.coords;//use scope portion then
-//            }
-//           //alert("Model: event:" + eventName + " " + JSON.stringify(model));
-//          }
-//        };
 
         $http.get(moodsBaseUrl)
         .success(function (data) {
             //moods = data;
-            $scope.moodMarkers = data.map(function(mood, idKey){
+            $scope.moodMarkers = data.map(function(mood){
                     return {
-                        title: mood.username,
                         latitude: mood.location[0],
                         longitude: mood.location[1],
-                        id: mood.id
-                    };             
+                        icon: getMoodIcon(mood.mood),
+                        id: mood.id,
+                        user: mood.username,
+                        mood: mood.mood,
+                        comment: mood.comment
+                    };
+                    
+                    function getMoodIcon(moodRate)
+                    {
+                        switch(moodRate){
+                            case 1: return "./img/smileyVeryHappy.png";
+                            case 2: return "./img/smileyGood.png";
+                            case 3: return "./img/smileySoSoLaLa.png";
+                            case 4: return "./img/smileyNotAmused.png";
+                            case 5: return "./img/smileyVeryMoody.png";
+                        };
+                    }
                 });
         })
         .error(function (data) {
